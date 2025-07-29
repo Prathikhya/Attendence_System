4abd9c63923686e618a30c3b115e2a013efdee25
prathikhya <prathikhyadevi25@gmail.com>
2025-07-29 15:28:43 +0530

    Success in register and login parts

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -3,10 +3,11 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
-  const { name, email, password, role } = req.body;
+  const { Username, email, password, role } = req.body;
+
   try {
     const hash = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hash, role });
+    const user = await User.create({ Username, email, password: hash, role });
     res.status(201).json({ message: 'User registered', user });
   } catch (err) {
     res.status(500).json({ error: 'Registration failed', details: err });
@@ -17,14 +18,22 @@ exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ where: { email } });
+    console.log("user found: ",user);
     if (!user) return res.status(401).json({ message: 'Invalid email' });
 
     const match = await bcrypt.compare(password, user.password);
+    console.log("password match: ",match);
+
     if (!match) return res.status(401).json({ message: 'Invalid password' });
 
     const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
     res.json({ token, user });
   } catch (err) {
+
+    console.error("Login error: ", err);
     res.status(500).json({ error: 'Login failed' });
   }
 };
+
+
+
--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,41 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
-// const cors = require('cors');
+const { sequelize } = require('./models');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth.routes');
+const attendanceRoutes = require('./routes/attendance.routes');
 
 
-dotenv.config();
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
-
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/attendance", attendanceRoutes);
+
+app.get('/home', (req, res) => {
+    res.send("Welcome to the army page");
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello Bts');
 });
-
-app.listen(port,() => {
+sequelize.authenticate()
+  .then(() => {
+    console.log("✅ Database connected successfully");
+    // Sync models (creates tables if not exist)
+    return sequelize.sync();
+  }).then(() => {
+    app.listen(port,() => {
     console.log("Server is Listening on the port " + port);
+    });
     
 })
+.catch(err => {
+    console.error("❌ Error connecting to database:", err);
+  });
